c73fc41b8d6977421b55c9a3c67c7060c182a5c6
spyro21 <38793743+spyro21@users.noreply.github.com>
2019-11-06 14:20:47 -0600

    first day of todo list you can add items to the todo list still cant edit the todo list though

--- ToDoList Brainstorm/Program.cs
+++ ToDoList Brainstorm/Program.cs
@@ -1,21 +1,53 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ToDoList_Brainstorm
 {
     class Program
     {
+        public string actionA;
+
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+           string[] toDoList = new string[0];
+            Array.Resize(ref toDoList, toDoList.Length + 2);
+            toDoList[0] = "egg moment";
+            toDoList[1] = "nice";
+            actionQ(toDoList);
         }
+        //
+        public static void actionQ(string[] arr) {
+            
+
+            string actionA;
+            string newItem;
+
+            listItems(arr);
+            Console.WriteLine("add or check?");
+            actionA = Console.ReadLine();
+
+            if (actionA == "add") {
+                Console.WriteLine("you chose add");
+                Console.WriteLine("what is the name of item?");
+
+                Array.Resize(ref arr, arr.Length + 1);
+                arr[arr.Length - 1] = Console.ReadLine();
+            } else if (actionA == "check")
+            {
+                Console.WriteLine("which item do you want to check off");
+            }
+            
+            actionQ(arr);
+        }
+
+        public static void listItems(string[] arr) {
+
+            Console.WriteLine("\nTo Do: ");
+
+            for (int i = 0; i < arr.Length;i++) {
+                Console.WriteLine((i + 1) + ".  " + arr[i]);
+            }
+            Console.WriteLine();
+        } 
     }
 }
